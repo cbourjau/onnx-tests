ec981bbda7a199c5dfa6f5ce7d88f2672afb262c
Christian Bourjau <christian.bourjau@quantco.com>
2025-08-10 19:52:40 +0200

    Add Cast test

--- onnx_tests/manipulation_functions.py
+++ onnx_tests/manipulation_functions.py
@@ -9,6 +9,52 @@ from onnx_tests._base_draw import TestCaseDraw
 
 
 @st.composite
+def cast(
+    draw: st.DrawFn, dtype_in: np.dtype, dtype_out: np.dtype, op: ModuleType
+) -> TestCaseDraw:
+    shape = draw(hyn.array_shapes(min_dims=1, min_side=0, max_dims=3))
+    if dtype_in.kind in "OU":
+        # strings
+        # TODO: The standard is very vague on strings!
+        if dtype_out is bool:
+            fun = np.vectorize(str)
+        elif dtype_out.kind == "f":
+            format_spec = draw(st.sampled_from(["e", "E", "f", "F"]))
+            fun = np.vectorize(lambda el: format(el, format_spec), otypes=[dtype_in])
+        elif dtype_out.kind in "iu":
+            fun = np.vectorize(str, otypes=[dtype_in])
+        elif dtype_out.kind == "b":
+            # TODO: Unclear how booleans should be formatted
+            fun = np.vectorize(str, otypes=[dtype_in])
+        elif dtype_out.kind in "OU":
+            # Noop
+            fun = np.vectorize(lambda el: el, otypes=[dtype_in])
+        else:
+            raise NotImplementedError
+
+        arr = fun(draw(h.arrays(dtype=dtype_out, shape=shape)))
+    elif dtype_in.kind == "f" and dtype_out.kind in "iu":
+        # Out-of-range is UB
+        min_val = float(np.asarray(np.iinfo(dtype_out).min, dtype_in))
+        max_val = float(np.asarray(np.iinfo(dtype_out).max, dtype_in))
+        arr = draw(
+            h.arrays(
+                dtype=dtype_in,
+                shape=shape,
+                max_value=max_val,
+                min_value=min_val,
+                allow_nan=False,
+            )
+        )
+    else:
+        # Booleans should work for any float input
+        arr = draw(h.arrays(dtype=dtype_out, shape=shape))
+    return TestCaseDraw(
+        inputs={"input": arr}, attribute_kwargs={"to": dtype_out}, spox_fun=op.cast
+    )
+
+
+@st.composite
 def concat(draw: st.DrawFn, dtype: np.dtype, op: ModuleType) -> TestCaseDraw:
     n_arrays = draw(st.integers(min_value=1, max_value=100))
     base_shape = draw(hyn.array_shapes(min_dims=1, min_side=0, max_dims=3))
--- tests/test_manipulation_functions.py
+++ tests/test_manipulation_functions.py
@@ -1,6 +1,41 @@
-from onnx_tests import manipulation_functions
+import numpy as np
+import pytest
+import spox.opset.ai.onnx.v21 as op
+from hypothesis import given
+from hypothesis import strategies as st
 
-from .utils import make_test as make_test
+from onnx_tests import helpers as h
+from onnx_tests import manipulation_functions
+from onnx_tests.config import run_candidate
+from onnx_tests.runtime_wrappers import run_reference
+
+from .utils import make_test
+
+
+@given(data=st.data())
+@pytest.mark.parametrize(
+    "dtype_out",
+    h.SCHEMAS["ai.onnx"]["Cast"][21].dtype_constraints["T2"],
+    ids=lambda el: f"to-{el}",
+)
+@pytest.mark.parametrize(
+    "dtype_in",
+    h.SCHEMAS["ai.onnx"]["Cast"][21].dtype_constraints["T1"],
+    ids=lambda el: f"from-{el}",
+)
+def test_Cast_21(data: st.DataObject, dtype_in, dtype_out):  # noqa
+    state = data.draw(manipulation_functions.cast(dtype_in, dtype_out, op))
+    model = state.build_model()
+
+    (cand,) = run_candidate(model).values()
+    (exp,) = run_reference(model).values()
+
+    if exp.dtype.kind in "UO":
+        # Strings must match exactly
+        np.testing.assert_array_equal(cand, exp)
+    else:
+        h.assert_allclose(cand, exp)
+
 
 make_test("Concat", 13, manipulation_functions.concat, globals())
 make_test("Compress", 11, manipulation_functions.compress, globals())
