cc2f2a3cc8f792d4b005b170101e9e0da98d0ed9
Christian Bourjau <christian.bourjau@quantco.com>
2025-08-22 08:42:41 +0200

    Add Det and Einsum

--- onnx_tests/linear_algebra_ops.py
+++ onnx_tests/linear_algebra_ops.py
@@ -2,9 +2,35 @@ from types import ModuleType
 
 import numpy as np
 from hypothesis import strategies as st
+from hypothesis.extra import numpy as hyn
 
 from onnx_tests import helpers as h
 from onnx_tests._base_draw import TestCaseDraw
+
+
+@st.composite
+def det(draw: st.DrawFn, dtype: np.dtype, op: ModuleType) -> TestCaseDraw:
+    shape = draw(hyn.array_shapes(min_dims=1, min_side=1))
+    shape = shape + (shape[-1],)
+    x = draw(h.arrays(dtype, shape=shape))
+    return TestCaseDraw(inputs={"X": x}, attribute_kwargs={}, spox_fun=op.det)
+
+
+@st.composite
+def einsum(draw: st.DrawFn, dtype: np.dtype, op: ModuleType) -> TestCaseDraw:
+    # TODO: Add more equations
+    # trace
+    equation = "ii"
+    shape = draw(hyn.array_shapes(min_dims=2, min_side=0))
+    arr = draw(h.arrays(dtype, shape))
+    if arr.ndim > 2:
+        equation = draw(st.sampled_from(["i...i", "ii...", "...ii"]))
+
+    return TestCaseDraw(
+        inputs={"Inputs": [arr]},
+        attribute_kwargs={"equation": equation},
+        spox_fun=op.einsum,
+    )
 
 
 @st.composite
--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -3,10 +3,13 @@ import pytest
 from hypothesis import given
 from hypothesis import strategies as st
 
+from onnx_tests import linear_algebra_ops as cases
 from onnx_tests.helpers import assert_allclose, run_candidate
-from onnx_tests.linear_algebra_ops import matmul
 
-from .utils import dtype_params, format
+from .utils import dtype_params, format, make_test
+
+make_test("Det", 22, cases.det, globals())
+make_test("Einsum", 12, cases.einsum, globals())
 
 
 @given(data=st.data())
@@ -15,7 +18,7 @@ def test_MatMul_13(data: st.DataObject, op, dtype):  # noqa
     # MatMul specs explicitly reference NumPy behavior as its semantics
     # onnxruntime fails on zero-sized inputs
 
-    state = data.draw(matmul(dtype, op))
+    state = data.draw(cases.matmul(dtype, op))
     model = state.build_model()
 
     expected = np.matmul(*state.inputs)  # type: ignore
